9b9dbc2f68485a9a2ea4a03655f45a7ad54a2290
Panda.com <J3fftw@gmail.com>
2020-02-23 16:38:20 +0100

    moved category

--- src/main/java/dev/j3fftw/soundmuffler/SoundMuffler.java
+++ src/main/java/dev/j3fftw/soundmuffler/SoundMuffler.java
@@ -1,20 +1,29 @@
 package dev.j3fftw.soundmuffler;
 
+import me.mrCookieSlime.Slimefun.Objects.Category;
 import me.mrCookieSlime.Slimefun.Objects.Research;
 import me.mrCookieSlime.Slimefun.api.Slimefun;
+import me.mrCookieSlime.Slimefun.api.SlimefunItemStack;
+import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class SoundMuffler extends JavaPlugin {
 
+    public static Category SOUND_MUFFLER;
+
     @Override
     public void onEnable() {
+        SOUND_MUFFLER = new Category(new NamespacedKey(this, "sound_muffler"),
+            new SlimefunItemStack("SOUND_MUFFLER", Material.BEACON, "&7SoundMuffler", "", "&a> Click to open"));
+
         new SoundMufflerListener(this).start();
 
         SoundMufflerMachine soundMufflerMachine = new SoundMufflerMachine();
         soundMufflerMachine.registerChargeableBlock(64);
 
-        Slimefun.registerResearch(new Research(new NamespacedKey(this, "sound_muffler"),6912, "Sound Muffler", 11),
+        Slimefun.registerResearch(new Research(new NamespacedKey(this, "sound_muffler"), 6912, "Sound Muffler", 11),
             soundMufflerMachine.getItem());
     }
+
 }
--- src/main/java/dev/j3fftw/soundmuffler/SoundMufflerMachine.java
+++ src/main/java/dev/j3fftw/soundmuffler/SoundMufflerMachine.java
@@ -30,7 +30,7 @@ public class SoundMufflerMachine extends SlimefunItem {
     private static final String id = "SOUND_MUFFLER";
 
     public SoundMufflerMachine() {
-        super(Categories.ELECTRICITY,
+        super(SoundMuffler.SOUND_MUFFLER,
             new SlimefunItemStack(id, Material.WHITE_CONCRETE, name,
                 "", "&7Muffles all sound in a", "&78 block radius"
             ),
